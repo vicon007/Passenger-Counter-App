8dac9a57dfd05450562019591e71a43d967bbc08
vicon007 <victoronotu@gmail.com>
2022-09-12 16:08:12 +0100

    Project files

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+let count = 0;
+
+let countEl = document.getElementById("count-el");
+
+let saveEl = document.getElementById("save-el");
+
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+};
+
+function save() {
+
+    let entries = count + " - ";
+    saveEl.textContent += entries;
+    countEl.textContent = 0;
+    count = 0;
+};
